e2fe02282b6881eca570f7ff812eede9fe0ebaa6
Brian Mullin <mullin.mm@gmail.com>
2018-04-29 13:17:30 +0200

    make minor cleanup

--- data/APIEndpoints.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  ordersApiEndpoint: "http://localhost:3000/orders/",
-  productsApiEndpoint: "http://localhost:3000/products/",
-  customersApiEndpoint: "http://localhost:3000/customers/",
-  dispatchOrderEndpoint: ""
-}
